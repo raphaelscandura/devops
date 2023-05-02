a8581a7f5ae54def98bc58fdab11da43983a6440
raphaelscandura <raphaelscandura@hotmail.com>
2023-05-02 12:24:08 -0300

    setting up, using ssh with wsl

---
+++ Infraestrutura como código - preparando máquinas na AWS com Ansible e Terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0a695f0d95cefc163"
+  instance_type = "t2.micro"
+  key_name = "MyFirstInstance"
+  tags = {
+    Name = "MyFirstInstance"
+  }
+}
